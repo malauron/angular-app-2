04c06adc1f89b68a542cee1c359fb7c18964af75
Mark <malauron@gmail.com>
2020-02-05 21:37:11 +0800

    Outputting a List of Recipes with ngFor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,6 +10,8 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     // tslint:disable-next-line:max-line-length
+    new Recipe('A text recipe', 'This is simply a test.', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2F14MgXgdennY%2Fhqdefault.jpg&f=1&nofb=1'),
+    // tslint:disable-next-line:max-line-length
     new Recipe('A text recipe', 'This is simply a test.', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2F14MgXgdennY%2Fhqdefault.jpg&f=1&nofb=1')
   ];
 
