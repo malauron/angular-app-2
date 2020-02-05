e902c385cdbde88935948c62de1d2c3f8757c87e
Mark <malauron@gmail.com>
2020-02-05 21:26:05 +0800

    Adding Content to the Recipes Components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
+
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    // tslint:disable-next-line:max-line-length
+    new Recipe('A text recipe', 'This is simply a test.', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2F14MgXgdennY%2Fhqdefault.jpg&f=1&nofb=1')
+  ];
 
   constructor() { }
 
