d3e0a59a6f4532d972d0e3ff38552ed29890f038
Mark <malauron@gmail.com>
2020-02-11 23:08:18 +0800

    Adding Ingredients to Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,29 @@
 import {EventEmitter} from '@angular/core';
 import {Recipe} from './recipe.model';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     // tslint:disable-next-line:max-line-length
-    new Recipe('A text recipe', 'This is simply a test.', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2F14MgXgdennY%2Fhqdefault.jpg&f=1&nofb=1'),
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super-tasty schnitzel - just awesome!',
+      'https://i.ytimg.com/vi/pc3YeZWwnLc/maxresdefault.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
     // tslint:disable-next-line:max-line-length
-    new Recipe('A text recipe2', 'This is simply a test2.', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2F14MgXgdennY%2Fhqdefault.jpg&f=1&nofb=1')
+    new Recipe(
+      'Big fat burger',
+      'What else you need to say!',
+      'https://i.ytimg.com/vi/xXFRe457uoI/maxresdefault.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
   ];
 
   getRecipes() {
