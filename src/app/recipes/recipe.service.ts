7b88cdd3597a59891a0a427c6d5f5662000d2057
Mark <malauron@gmail.com>
2020-02-11 21:50:48 +0800

    Using a Service for Cross-Component Communication

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
+import {EventEmitter} from '@angular/core';
 import {Recipe} from './recipe.model';
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     // tslint:disable-next-line:max-line-length
     new Recipe('A text recipe', 'This is simply a test.', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2F14MgXgdennY%2Fhqdefault.jpg&f=1&nofb=1'),
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -10,12 +10,16 @@ import {RecipeService} from './recipe.service';
 })
 export class RecipesComponent implements OnInit {
   selectedRecipe: Recipe;
-  constructor() { }
+
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipeService.recipeSelected
+      .subscribe(
+        (recipe: Recipe) => {
+          this.selectedRecipe = recipe;
+        }
+      );
   }
 
-  onSelectedRecipe(recipe: Recipe) {
-    this.selectedRecipe = recipe;
-  }
 }
