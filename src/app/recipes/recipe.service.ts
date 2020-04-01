7cd814b2f5dc7be4d901cf809090b3f448649564
Mark <malauron@gmail.com>
2020-04-01 12:56:52 +0800

    Fixing a Bug with the Resolver

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,36 +1,37 @@
-import {Injectable} from '@angular/core';
-import {Recipe} from './recipe.model';
-import {Ingredient} from '../shared/ingredient.model';
-import {ShoppingListService} from '../shopping-list/shopping-list.service';
-import {Subject} from 'rxjs';
+import { Injectable } from "@angular/core";
+import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { Subject } from "rxjs";
 
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    // tslint:disable-next-line:max-line-length
-    new Recipe(
-      'Tasty Schnitzel',
-      'A super-tasty schnitzel - just awesome!',
-      'https://i.ytimg.com/vi/pc3YeZWwnLc/maxresdefault.jpg',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('French Fries', 20)
-      ]),
-    // tslint:disable-next-line:max-line-length
-    new Recipe(
-      'Big fat burger',
-      'What else you need to say!',
-      'https://i.ytimg.com/vi/xXFRe457uoI/maxresdefault.jpg',
-      [
-        new Ingredient('Buns', 2),
-        new Ingredient('Meat', 1)
-      ])
-  ];
+  // private recipes: Recipe[] = [
+  //   // tslint:disable-next-line:max-line-length
+  //   new Recipe(
+  //     "Tasty Schnitzel",
+  //     "A super-tasty schnitzel - just awesome!",
+  //     "https://i.ytimg.com/vi/pc3YeZWwnLc/maxresdefault.jpg",
+  //     [new Ingredient("Meat", 1), new Ingredient("French Fries", 20)]
+  //   ),
+  //   // tslint:disable-next-line:max-line-length
+  //   new Recipe(
+  //     "Big fat burger",
+  //     "What else you need to say!",
+  //     "https://i.ytimg.com/vi/xXFRe457uoI/maxresdefault.jpg",
+  //     [new Ingredient("Buns", 2), new Ingredient("Meat", 1)]
+  //   )
+  // ];
 
-  constructor(private slService: ShoppingListService) {
+  private recipes: Recipe[] = [];
 
+  constructor(private slService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
   }
 
   getRecipes() {
@@ -41,7 +42,7 @@ export class RecipeService {
     return this.recipes[id];
   }
 
-  addIngredientsToShoppingList(ingredients: Ingredient[]){
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
   }
 
